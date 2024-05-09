4eaabda53bc1417c2e597e55ab8cf9a8723d4e59
Destiny Moncrief <D.moncrief4@gmail.com>
2024-05-09 14:02:23 -0400

    made changes

--- scripts/site.js
+++ scripts/site.js
@@ -1,18 +1,10 @@
 "use strict"
 
-document.getElementById("costEstimatorForm").addEventListener("submit", function(event) {
-     event.preventDefault();  
-
-
-}
 
 // Get form data
 
 const name = document.getElementById("name").value;
 const email = document.getElementById("email").value;
-const checkInDate = document.getElementById("checkInDate").value;
-const numNights = document.getElementById("numNights").value;
-const roomType = document.getElementById("").value;
-const numAdults = document.getElementById("").value;
-const numChildren = document.getElementById("name").value;
-const discount = document.getElementById("name").value;
+
+
+

20e3a7f2731fdf4200852ef687d21bea0e07ec8c
Destiny Moncrief <D.moncrief4@gmail.com>
2024-05-10 13:58:20 -0400

    saved changes

--- scripts/site.js
+++ scripts/site.js
@@ -1,30 +1,27 @@
 "use strict"
 
 
-document.getElementById("costsEstimator").addEventListener"submit" , function(event) {
-    event.preventDefault();   //prevent the form from submitting
+document.getElementById("costsEstimator") , function(event) {
+    
 
 }
 
 // Get form data
 
-const name = document.getElementById("name").value;
+const name = document.getElementById("name");
 const email = document.getElementById("email").value;
-const checkInDate = new Date(document.getElementById("checkinDate").value);
+const checkInDate = new Date(document.getElementById("checkinDate");
 const numNights = parseInt(document.getElementById("numNights").value);
-const roomType = document.querySelector('input[name="roomType"]:checked').value;
+const roomType = documentr('input[name="roomType"]:checked');
 const numAdults = parseInt(document.getElementById("numAdults").value);
-const numChildren = parseInt(document.getElementById("numChildren").value);
-const discount = document.querySelector('input[name="discount"]:checked').value;
+const numChildren = document.getElementById("numChildren");
+const discount = ["discount"]
 
 
 // validate form data
 
 const messageDiv = document.getElementById("messageDiv");
-if(!validateFormData)(name,email,checkInDate, numNights, roomType, numAudits, numChildren, discount)) {
-    messageDiv.innerText = "Invalid input. Please check your entries";
-    return;
-}
+
 
 // calculate stay cost
 
@@ -35,8 +32,6 @@ const totalCost = calculateTotalCost(discountRoomRate, tax);
 
 
 // Display results
-messageDiv.innerHTML = 
-<p>Original </p>
 
 
 

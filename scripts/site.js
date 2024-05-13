c09fe05e92923b59f710adba0c5edcb8e9991fa8
Destiny Moncrief <D.moncrief4@gmail.com>
2024-05-12 22:34:13 -0400

    made changes

--- scripts/site.js
+++ scripts/site.js
@@ -1,38 +1,42 @@
 "use strict"
 
 
-document.getElementById("costsEstimator") , function(event) {
-    
-
-}
-
-// Get form data
-
-const name = document.getElementById("name");
-const email = document.getElementById("email").value;
-const checkInDate = new Date(document.getElementById("checkinDate");
-const numNights = parseInt(document.getElementById("numNights").value);
-const roomType = documentr('input[name="roomType"]:checked');
-const numAdults = parseInt(document.getElementById("numAdults").value);
-const numChildren = document.getElementById("numChildren");
-const discount = ["discount"]
 
 
-// validate form data
+    // Get form data
+    const name = document.getElementById("name");
+    const email = document.getElementById("email").value;
+    const checkInDate = new Date(document.getElementById("checkInDate")).value;
+    const numnNights = parseInt(document.getElementById("numNights").value);
+    const roomType = document.querySelector('input[name="roomType"]:checked').value;
+    const numAdults = parseInt(document.getElementById("numAdults").value);
+    const nuumChildren = parseInt(document.getElementById("numChildren").value);
+    const discount = document.querySelector('input[name="discount"]:checked').value;
 
-const messageDiv = document.getElementById("messageDiv");
+    // calulate stay cost 
+    const roomRate = getRoomRate(checkInDate, roomType);
+    const discountPerecent = getDiscountPercent(discount);
+    const discountedRoomRate = calculateDiscountedRoomRate(roomRate, discountPercent);
+    const tax = calulateTax(discountedRoomRate);
+    const totalCost = calculateTotalCost(discountedRoomRate, tax);
+
+    // Display Results
+    const messageDiv = document.getElementById("messageDive");
+    messageDiv.innerHTML = `
+    <p> Original Room Cost: $${roomRate.toFixed(2)}</p>
+    <p> Discount: ${discountPercent}%</p>
+    <p> Discounted Room Cost: $${discountedRoomrate.toFixed(2)}</p>
+    <p> Tax: $${tax.toFixed(2)}</p>
+    <p> Total Cost of Stay: $${totalCost.toFixed(2)}</p>
+    `;
+ 
+
+    function getRoomRate(checkInDate, roomType) {
+        return 150.00;
+    }
 
 
-// calculate stay cost
+     function getDiscountPercent(discount) {
 
-const roomRate = getroomRate(checkInDate, roomType);
-const discountPercent = getDiscountPercent(discount);
-const discountedRoomRate = calculateDiscountedRoomRate(roomRate, discountPercent)
-const totalCost = calculateTotalCost(discountRoomRate, tax);
-
-
-// Display results
-
-
-
-
+     }
+   
